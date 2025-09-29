9b910ca57c65f19f9b0a045f3f732cf1a5e51c6f
Dounia Bahzad <douniabahzad@gmail.com>
2025-09-29 11:28:23 +0200

    29sept

--- src/main.js
+++ src/main.js
@@ -5,17 +5,19 @@ import { RightScreen } from './timeline/AIRight';
 import { GameScreen } from './timeline/GameScreen';
 
 const socket = io("http://localhost:3000");
+// const socket = io("https://pong-battle-production.up.railway.app");
 let timeline = new RightScreen;
 // timeline.play();
 
-const btnRole1 = document.querySelector("#role1")
-const btnRole2 = document.querySelector("#role2")
-const btnRole3 = document.querySelector("#role3")
-const ready = document.querySelector("#ready")
+const btnRole1 = document.querySelector("#left")
+const btnRole2 = document.querySelector("#game")
+const btnRole3 = document.querySelector("#right")
+
 
 function selectRole(role) {
   console.log(`Je suis ${role}`)
   socket.emit("joinRoom", role)
+  socket.emit("ready", ready)
 
   switch(role){
     case "LeftScreen":
@@ -47,5 +49,3 @@ socket.on("start", ({time}) => {
 btnRole1.addEventListener("click", () => selectRole("LeftScreen"));
 btnRole2.addEventListener("click", () => selectRole("RightScreen"));
 btnRole3.addEventListener("click", () => selectRole("GameScreen"));
-ready.addEventListener("click", setReady);
-
